4fb7282990835153b4580a6d1b7e11f9aa1c785b
Aayush <ks12031982@gmail.com>
2026-09-11 00:49:35 +0530

    Time: 6 ms (87.45%) | Memory: 43.8 MB (85.30%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int n = s1.length(), m = s2.length();
-        if (n > m)
-            return false;
+        if (n > m) return false;
 
         int[] need = new int[26];
         int[] window = new int[26];
@@ -12,37 +11,22 @@ class Solution {
             window[s2.charAt(i) - 'a']++;
         }
 
-        int matches = 0;
-        for (int i = 0; i < 26; i++) {
-            if (need[i] == window[i])
-                matches++;
-        }
-
-        if (matches == 26)
-            return true;
+        if (matches(need, window)) return true;
 
         for (int i = n; i < m; i++) {
-            int addChar = s2.charAt(i) - 'a';
-            int removeChar = s2.charAt(i - n) - 'a';
+            window[s2.charAt(i) - 'a']++;              // add new right char
+            window[s2.charAt(i - n) - 'a']--;           // remove left char that fell off
 
-            // Add new right character
-            window[addChar]++;
-            if (window[addChar] == need[addChar])
-                matches++;
-            else if (window[addChar] == need[addChar] + 1)
-                matches--;
-
-            // Remove left character
-            window[removeChar]--;
-            if (window[removeChar] == need[removeChar])
-                matches++;
-            else if (window[removeChar] == need[removeChar] - 1)
-                matches--;
-
-            if (matches == 26)
-                return true;
+            if (matches(need, window)) return true;
         }
 
         return false;
+    }
+
+    private boolean matches(int[] need, int[] window) {
+        for (int i = 0; i < 26; i++) {
+            if (need[i] != window[i]) return false;
+        }
+        return true;
     }
 }
